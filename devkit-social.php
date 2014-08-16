7ffc5defd67c4cf15a5ead205d9837d6de8e918e
ThinkDualBrain <brian@thinkdualbrain.com>
2014-08-16 01:39:20 -0400

    Initial commit

---
+++ devkit-social.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Devkit Social
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A suite of social media sharing tools for WordPress developers. Not for the average user, PHP and HTML/CSS knowledge is necessary for using this plugin!
+Version: 0.1
+Author: Devkit
+Author URI: http://devkit.tools
+License: A "Slug" license name e.g. GPL2
+*/
